a1fee9067b646b25158d1be1701c498c9cf8db78
include-yy <include_yy@qq.com>
2025-08-05 19:09:44 +0800

    Revert bdaff41.

--- ox-w3ctr.el
+++ ox-w3ctr.el
@@ -3174,8 +3174,7 @@ tags to be included in the HTML head."
    (when-let* ((desc (t--nw-trim (t--pget info :description))))
      (list "name" "description" desc))
    (when-let* ((keyw (t--nw-trim (t--pget info :keywords))))
-     (list "name" "keywords"
-           (string-join (string-split keyw " ") ", ")))
+     (list "name" "keywords" keyw))
    '("name" "generator" "Org Mode")))
 
 (defun t--build-meta-tags (info)
