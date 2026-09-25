3f077861a9264dd11ebc60b6bcf0971b55b4ae38
Yue Yi <include_yy@qq.com>
2026-09-26 05:26:08 +0800

    v0.2.12: Headline refactoring complete

--- ox-w3ctr.el
+++ ox-w3ctr.el
@@ -6,7 +6,7 @@
 ;; Maintainer: include-yy <yy@egh0bww1.com>
 ;; Created: 2024-03-18 04:51:00+0900
 
-;; Package-Version: 0.2.11
+;; Package-Version: 0.2.12
 ;; Package-Requires: ((emacs "31"))
 ;; Keywords: tools, html
 ;; URL: https://github.com/include-yy/ox-w3ctr
@@ -50,7 +50,7 @@
 (require 'table)
 
 ;;;; Fundamental utilities
-(defconst t-version "0.2.11"
+(defconst t-version "0.2.12"
   "The current version string of the ox-w3ctr package.")
 
 (defconst t--dir
